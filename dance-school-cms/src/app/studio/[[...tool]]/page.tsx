006ee1dee75df7cb8ff8cfa14ee41161b130f326
theothecoder <theothecoder@gmail.com>
2026-03-02 12:08:44 +0100

    fix: replace embedded Sanity Studio with redirect to hosted studio - eliminates 1.42MB bundle causing Vercel deploy failure

--- dance-school-cms/src/app/studio/[[...tool]]/page.tsx
+++ dance-school-cms/src/app/studio/[[...tool]]/page.tsx
@@ -1,19 +1,11 @@
-/**
- * This route is responsible for the built-in authoring environment using Sanity Studio.
- * All routes under your studio path is handled by this file using Next.js' catch-all routes:
- * https://nextjs.org/docs/routing/dynamic-routes#catch-all-routes
- *
- * You can learn more about the next-sanity package here:
- * https://github.com/sanity-io/next-sanity
- */
-
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import { redirect } from 'next/navigation'
 
 export const dynamic = 'force-dynamic'
 
-export { metadata, viewport } from 'next-sanity/studio'
-
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+  if (projectId) {
+    redirect(`https://${projectId}.sanity.studio/`)
+  }
+  redirect('https://sanity.io/manage')
 }
